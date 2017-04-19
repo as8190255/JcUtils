c9be759b7e8d655d07cc44447f2487053c533118
as8190255 <355225537@qq.com>
2017-04-20 01:14:49 +0800

    65.1 65.2 协议的模型初始参数更换

---
+++ app/src/main/java/org/jc/jcutils/config/Config.java
@@ -0,0 +1,14 @@
+package org.jc.jcutils.config;
+
+/**
+ * 配置信息
+ * Created by a3552 on 2017/4/20.
+ */
+
+public class Config {
+
+    /** 屏幕高度*/
+    public static int PHONE_HEIGHT = 1920;
+    /** 屏幕宽度*/
+    public static int PHONE_WITH = 1080;
+}
--- app/src/main/java/org/jc/jcutils/javabean/Rule65_2Send.java
+++ app/src/main/java/org/jc/jcutils/javabean/Rule65_2Send.java
@@ -1,5 +1,6 @@
 package org.jc.jcutils.javabean;
 
+import org.jc.jcutils.config.Config;
 import org.jc.jcutils.utils.ByteUtils;
 
 /**
@@ -8,6 +9,30 @@ import org.jc.jcutils.utils.ByteUtils;
  */
 
 public class Rule65_2Send extends SocketWrite{
+    /**
+     *
+     * @param address 地址
+     * @param sx 屏幕开始x轴坐标
+     * @param sy 屏幕开始y轴坐标
+     * @param ex 屏幕结束y轴坐标
+     * @param ey 屏幕结束y轴坐标
+     */
+    public Rule65_2Send(String address, int sx, int sy, int ex, int ey) {
+        super(address, display(new byte[]{65, 1, 0, 0, 0, 0, 0, 0, 0, 0}, sx, sy, ex, ey));
+    }
+    public static byte[] display(byte[]src, int sx, int sy, int ex, int ey){
+        int sxi = (int) (sx * 1.0f / Config.PHONE_WITH * 10000);
+        int syi = (int) (sy * 1.0f / Config.PHONE_HEIGHT * 10000);
+        int exi = (int) (ex * 1.0f / Config.PHONE_WITH * 10000);
+        int eyi = (int) (ey * 1.0f / Config.PHONE_HEIGHT * 10000);
+
+        src = ByteUtils.int16s2byteArrays(sxi, src, 2);
+        src = ByteUtils.int16s2byteArrays(syi, src, 4);
+        src = ByteUtils.int16s2byteArrays(exi, src, 6);
+        src = ByteUtils.int16s2byteArrays(eyi, src, 8);
+        return src;
+    }
+
     /**
      *
      * @param address 地址
